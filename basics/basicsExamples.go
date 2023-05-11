5cbc52bcfc4c122b023c637856a7ead22659a720
Artem Galushko <galushko@my.com>
2023-05-11 14:50:02 +0600

    Small update for slices Move logic out of main

---
+++ basics/basicsExamples.go
@@ -0,0 +1,23 @@
+package basics
+
+import "fmt"
+
+func Basics() {
+	fmt.Println("Hello world")
+	Variables()
+	Conditions(25)
+	Iterations()
+	counter := Counter()
+	fmt.Println(counter())
+	fmt.Println(counter())
+	fmt.Println(counter())
+	Pointers()
+	Slices()
+	Maps()
+	Panics()
+	Structures()
+	Interfaces()
+	month := May
+	Enums(&month)
+	Generics()
+}
